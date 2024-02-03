a6fc0bd83fdaab24420e36ec9b11ac28de59531d
Masha Baidachna <realcowcitizen@gmail.com>
2024-02-03 20:37:52 +0000

    initial commit

---
+++ src/prompt_script.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+import api_key
+
+API_KEY = api_key.get_key()
+
+client = OpenAI(api_key=API_KEY)
+
+stream = client.chat.completions.create(
+    model="gpt-3.5-turbo-0125",
+    messages=[{"role": "user", "content": "Say this is a test"}],
+    stream=True,
+)
+
+for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end="")
+        
